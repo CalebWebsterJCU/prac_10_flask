ea0ed07c773499f2aabbd31fb2f3de1d402eb537
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-10-28 19:43:32 +1000

    Add greet() function Test

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():
     return '<h1>Hello World :)</h1>'
 
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name="Caleb Webster"):
+    return f"<h1>Hello {name}</h1>"
+
+
 if __name__ == '__main__':
     app.run()
