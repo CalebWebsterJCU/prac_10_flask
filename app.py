da2b3b2eb2106c7339389380bf404dd1525cd545
Caleb Webster <caleb.webster@my.jcu.edu.au>
2020-10-28 19:43:17 +1000

    Add app.py

--- app.py
+++ app.py
@@ -8,23 +8,5 @@ def hello_world():
     return '<h1>Hello World :)</h1>'
 
 
-@app.route('/greet')
-@app.route('/greet/<name>')
-def greet(name="default"):
-    return f"<h1>Hello {name}</h1>"
-
-
-@app.route('/f-c/<fahrenheit>')
-def fahrenheit_to_celsius(fahrenheit=32.0):
-    """Convert Fahrenheit to Celsius."""
-    return f"<h2>F: {fahrenheit}° C: {5 / 9 * (float(fahrenheit) - 32)}°</h2>"
-
-
-@app.route('/c-f/<celsius>')
-def celsius_to_fahrenheit(celsius=0.0):
-    """Convert Celsius to Fahrenheit."""
-    return f"<h2>C: {celsius}° F: {float(celsius) * 9.0 / 5 + 32}°</h2>"
-
-
 if __name__ == '__main__':
     app.run()
